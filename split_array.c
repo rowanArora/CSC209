3e7d0ce7b63fd9ba9c94761b6277a9f2a19f7d42
rowanArora <rowan.arora@mail.utoronto.ca>
2023-01-27 11:57:07 -0800

    First attempt at completing the methods within the split_array.c file! Hopefully this works.

--- split_array.c
+++ split_array.c
@@ -12,6 +12,54 @@
 */
 int **split_array(const int *s, int length) {
 
+  int **ptr;
+  size_t even = 0;
+  int evenCounter = 0;
+  size_t odd = 1;
+  int oddCounter = 0;
+
+  ptr = malloc(sizeof(sizeof(int) * 2));
+
+  if (ptr) {
+
+    if (length % 2 == 0) {
+
+      int arrayLength = length/2;
+
+      ptr[even] = malloc(sizeof(*ptr[even]) * arrayLength);
+      ptr[odd] = malloc(sizeof(*ptr[odd]) * arrayLength);
+
+    }
+    else {
+
+      int evenArrayLength = (length/2) + 1;
+      int oddArrayLength = length/2;
+
+      ptr[even] = malloc(sizeof(*ptr[even]) * evenArrayLength);
+      ptr[odd] = malloc(sizeof(*ptr[odd]) * oddArrayLength);
+
+    }
+
+    for (int x = 0; x < length; ++x) {
+
+      if (x % 2 == 0) {
+
+        ptr[even][evenCounter] = s[x];
+        evenCounter++;
+
+      }
+      else {
+
+        ptr[odd][oddCounter] = s[x];
+        oddCounter++;
+
+      }
+
+    }
+
+  }
+
+  return ptr;
 
 }
 
@@ -23,6 +71,16 @@ int **split_array(const int *s, int length) {
 
 int *build_array(char **strs, int size) {
 
+  int *ptr = malloc(sizeof(int) * size);
+
+  for (int x = 0; x < size; ++x) {
+
+    int current = atoi(strs[x + 1]);
+    ptr[x] = current;
+
+  }
+
+  return ptr;
 
 }
 
@@ -32,8 +90,8 @@ int main(int argc, char **argv) {
        arguments.  Do not add any additional lines of code to the main
        function or make other changes.
      */
-    int *full_array = build_array(/* fill in the arguments*/);
-    int **result = split_array(full_array, /* fill in this argument */);
+    int *full_array = build_array(argv, argc - 1);
+    int **result = split_array(full_array, sizeof(*full_array));
 
     printf("Original array:\n");
     for (int i = 0; i < argc - 1; i++) {
